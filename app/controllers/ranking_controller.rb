26478cbb27d735fe19943add9a574b15d24d0518
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-17 11:08:14 +0000

    edit user page

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,17 @@
+class RankingController < ApplicationController
+  
+  def have
+    # -- type = "Have"を検索してレコードを絞り込み item_idをカウントしグループ化
+    @best_items_id=Ownership.where(type: "Have").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    #binding.pry    
+    @items=Item.find(@best_items_id)
+    #binding.pry
+  end
+  
+  def want
+    @best_items_id=Ownership.where(type: "Want").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    @items=Item.find(@best_items_id)
+  end
+  
+end
+
