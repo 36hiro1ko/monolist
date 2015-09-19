833983c184aa761753979d1b11ac0b272c434e25
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-19 08:09:08 +0000

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Monolist
   class Application < Rails::Application
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    #config.assets.paths << "#{Rails}/vendor/assets/fonts"
   
     config.active_record.raise_in_transactional_callbacks = true
     config.generators do |g|
