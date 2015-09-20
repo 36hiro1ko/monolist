93f6c689c0ff8d6a12650705a2b077e8bf8bdafe
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-20 06:25:14 +0000

    edit sql

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -5,11 +5,12 @@ class RankingController < ApplicationController
   
   def have
     # -- type = "Have"を検索してレコードを絞り込み item_idをカウントしグループ化
-    #best_items_id=Ownership.where(type: "Have").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    #@best_items_id=Ownership.where(type: "Have").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
   
-    # -- 分からないので生SQLを書いてみました　--
-    @items=Item.find_by_sql('select * ,count(item_id) AS item_id_count from ownerships join items ON ownerships.item_id = items.id where type = \'Have\' group by item_id order by count(item_id) desc')
+    # -- 生SQLを書いてみました　--
+    @items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = "Have" order by count(item_id) desc')
     
+
     #@items=Item.find(@best_items_id)
 
     #binding.pry
@@ -17,11 +18,11 @@ class RankingController < ApplicationController
   end
   
   def want
-    # @best_items_id=Ownership.where(type: "Want").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-    # @items=Item.find(@best_items_id)
+    #@best_items_id=Ownership.where(type: "Want").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    #@items=Item.find(@best_items_id)
     
-    # -- 分からないので生SQLを書いてみました　--
-    @items=Item.find_by_sql('select * ,count(item_id) AS item_id_count from ownerships join items ON ownerships.item_id = items.id where type = \'Want\' group by item_id order by count(item_id) desc')
+    # --- 生SQLを書いてみました　--
+   @items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = "Want" order by count(item_id) desc')
   end
   
 end
