c5361e3014ee75fe2c6be521aeed784594bcf8f1
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-20 04:05:44 +0000

    edit sql

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -8,7 +8,7 @@ class RankingController < ApplicationController
     #best_items_id=Ownership.where(type: "Have").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
   
     # -- 分からないので生SQLを書いてみました　--
-    @items=Item.find_by_sql('select *,count(item_id) AS item_id_count from ownerships join items ON ownerships.item_id = items.id where type="Have" group by item_id order by count(item_id) desc')
+    @items=Item.find_by_sql('select *,count(item_id) AS item_id_count from ownerships join items ON ownerships.item_id = items.id where type = \'Have\' group by item_id order by count(item_id) desc')
     
     #@items=Item.find(@best_items_id)
 
@@ -21,7 +21,7 @@ class RankingController < ApplicationController
     # @items=Item.find(@best_items_id)
     
     # -- 分からないので生SQLを書いてみました　--
-    @items=Item.find_by_sql('select *,count(item_id) AS item_id_count from ownerships join items ON ownerships.item_id = items.id where type="Want" group by item_id order by count(item_id) desc')
+    @items=Item.find_by_sql('select *, count(item_id) AS item_id_count from ownerships join items ON ownerships.item_id = items.id where type = \'Want\' group by item_id order by count(item_id) desc')
   end
   
 end
