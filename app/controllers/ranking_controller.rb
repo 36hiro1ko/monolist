19fe3cfb9c8956151108c3ead66781f7aee0180b
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-20 07:00:28 +0000

    edit sql

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -6,12 +6,9 @@ class RankingController < ApplicationController
   def have
     # -- type = "Have"を検索してレコードを絞り込み item_idをカウントしグループ化
     #@best_items_id=Ownership.where(type: "Have").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-  
-    # -- 生SQLを書いてみました　--
-    @items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = \'Have\' order by count(item_id) desc')
-    
-
     #@items=Item.find(@best_items_id)
+    # -- 生SQLを書いてみました　--
+    @items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = \'Have\' order by count(item_id) desc limit 10')
 
     #binding.pry
 
@@ -22,7 +19,7 @@ class RankingController < ApplicationController
     #@items=Item.find(@best_items_id)
     
     # --- 生SQLを書いてみました　--
-   @items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = \'Want\' order by count(item_id) desc')
+   @items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = \'Want\' order by count(item_id) desc limit 10')
   end
   
 end
