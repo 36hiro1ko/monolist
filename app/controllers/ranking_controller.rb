a3cd8633d9f41cab61c2b6f995e4d531811e5ca0
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-21 08:47:03 +0000

    edit stylesheet

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -5,21 +5,21 @@ class RankingController < ApplicationController
   
   def have
     # -- type = "Have"を検索してレコードを絞り込み item_idをカウントしグループ化
-    #@best_items_id=Ownership.where(type: "Have").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-    #@items=Item.find(@best_items_id)
+    best_items_id=Ownership.where(type: "Have").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    @items=Item.find(best_items_id).sort_by{|v| best_items_id.index(v.id)}
     # -- 生SQLを書いてみました　--
-    @items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = \'Have\' order by count(item_id) desc limit 10')
+    #@items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = \'Have\' order by count(item_id) desc limit 10')
 
     #binding.pry
 
   end
   
   def want
-    #@best_items_id=Ownership.where(type: "Want").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-    #@items=Item.find(@best_items_id)
+    best_items_id=Ownership.where(type: "Want").group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    @items=Item.find(best_items_id).sort_by{|v| best_items_id.index(v.id)}
     
     # --- 生SQLを書いてみました　--
-   @items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = \'Want\' order by count(item_id) desc limit 10')
+   #@items=Item.find_by_sql('select items.id, item_id, title, asin, large_image, type, count(item_id) from ownerships join items on ownerships.item_id = items.id  GROUP BY item_id, items.id, title, asin,large_image, type HAVING type = \'Want\' order by count(item_id) desc limit 10')
   end
   
 end
