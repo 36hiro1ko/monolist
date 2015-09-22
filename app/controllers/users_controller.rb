8a7477b29f17a69d4ccc713a0c8edf2428deaa17
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-22 10:55:07 +0000

    edit user_controller fot show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,10 @@ class UsersController < ApplicationController
   end
   
   def show
-    #@items = @user.items
-    @items = @user.items.group("item_id")
+    @items = @user.items 
+    #wantとhaveをされた場合に重複してアイテムが表示されないように以下を指定
+    #しかし、pg環境下ではエラーになるようなのでコメントアウトとする
+    #@items = @user.items.group("item_id")
   end
 
   private
